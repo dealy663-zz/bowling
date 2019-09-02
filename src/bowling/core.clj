e90249e3466d37928a2c7ab27ebfa73051c46026
Derek Ealy <dealy663@gmail.com>
2019-09-02 10:11:03 -0700

    basic setup, tests and main are running

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,6 +1,17 @@
 (ns bowling.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def frame {:ball1 0
+            :ball2 0})
+
+(def score-card {:frames (into [] (take 10 (repeat frame)))
+                 :score  0})
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Use Ctrl+c to exit.")
+
+  (while true
+    (let [cmd (slurp *in*)]
+      (println (str "you entered: " cmd)))))
