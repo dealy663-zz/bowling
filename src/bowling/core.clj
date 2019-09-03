9533fae7df0c768991e8a9a25c0c819c39598a51
Derek Ealy <dealy663@gmail.com>
2019-09-02 20:11:47 -0700

    Basic bowling game working

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -3,8 +3,7 @@
 ;; may want to do more than just track the pins knocked down not sure yet
 (def frame {:ball1 0
             :ball2 0
-            :ball3 nil
-            :score 0})
+            :ball3 nil})
 
 
 ;; Assuming a standard game of 10 frames, prob will just manage the spares and
@@ -62,12 +61,30 @@
     (throw (Exception. "ball3 can only be thrown in the final frame when ball1 is a strike"))
 
     (or (> ball1 10) (< ball1 0)
-        (> ball2 10) (< ball2 0)
-        (> ball3 10) (< ball3 0))
+        (> ball2 10) (< ball2 0))
+    (throw (Exception. "A ball value can only be between 0 and 10"))
+
+    (and (not (nil? ball3))
+         (or (> ball3 10) (< ball3 0)))
     (throw (Exception. "A ball value can only be between 0 and 10"))
 
     (>= (-> score-card :frames count) 10)
     (throw (Exception. "The game is over we hope you enjoyed your game at Rokt Bowling."))))
+
+(defn update-penultimate-frame
+  "update the values of the second to last frame
+
+   score-card       The score card representing the game being played
+   current-frame    The scores for the frame that was just bowled"
+  [score-card current-frame]
+
+  (let [cc2 (-> score-card :frames count (- 2))]
+    (if (>= cc2 0)
+      (let [pen-frame (-> score-card :frames (nth cc2))]
+        (if (= 10 (:ball1 pen-frame))
+          (update-in score-card [:frames cc2 :score] + (:ball1 current-frame))
+          score-card))
+      score-card)))
 
 (defn update-last-frame
   "update the values of the previous frame
@@ -78,12 +95,16 @@
    returns the score-card with the last frame with the updated score if there was a strike or spare"
   [score-card current-frame]
 
-  (let [last-frame (-> score-card :frames last)]
-    (if (= 10 (:ball1 last-frame))                          ;; there was a strike
-      (update-in last-frame [:score] (+ (:ball1 current-frame) (:ball2 current-frame)))
-      (if (= 10 (:ball2 last-frame))                        ;; the spare was picked up
-        (update-in last-frame [:score] (+ (:ball1 current-frame)))
-        score-card))))
+  (let [cc (-> score-card :frames count dec)]               ;; nothing to update if 1st frame
+    (if (>= cc 0)
+      (let [last-frame (-> score-card :frames last)]
+        (if (= 10 (:ball1 last-frame))                      ;; there was a strike
+          (let [new-sc (update-in score-card [:frames cc :score] + (:ball1 current-frame) (:ball2 current-frame))]
+            (update-penultimate-frame new-sc current-frame))
+          (if (= 10 (:score last-frame))                    ;; the spare was picked up if score is 10 but ball1 wasn't
+            (update-in score-card [:frames cc :score] + (:ball1 current-frame))
+            score-card)))
+      score-card)))
 
 (defn score-frame
   "Scores a bowled frame.
@@ -102,16 +123,23 @@
 
   ([score-card ball1 ball2 ball3]
    (try
-     (validate score-card ball1 ball2 ball3)
+     (validate score-card ball1 ball2 ball3)                     ;; check inputs
 
-     (let [new-frame (assoc frame :ball1 ball1 :ball2 ball2 :ball3 ball3)
-           last-frame (update-last-frame score-card new-frame)]
-       (update-in score-card [:frames] conj new-frame)
-       )
+     (let [ball3      (if (nil? ball3) 0 ball3)
+           new-frame  (assoc frame :ball1 ball1 :ball2 ball2 :ball3 ball3
+                                   :score (+ ball1 ball2 ball3)) ;; setup new frame
+           new-sc     (update-last-frame score-card new-frame)]  ;; update previous frames
+       (update-in new-sc [:frames] conj new-frame))              ;; add new frame to score-card
      (catch Exception exp
-       (assoc score-card :last-exception exp)))
+       (assoc score-card :last-exception exp)))))
 
-   ))
+(defn get-total-score
+  "Returns the current score of the game, total of all frames
+
+   score-card   The score card representing the game being played"
+  [score-card]
+
+  (reduce + 0 (map #(:score %) (:frames score-card))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -65,6 +65,68 @@
 
       (let [nsc (score-frame final-frame 10 1 4)
             exp (:last-exception nsc)]
-        (is (nil? exp)))
+        (is (nil? exp))))))
 
-      )))
+(deftest update-tester
+  (testing "update-last-frame spare"
+    (let [sc1 (update-in score-card [:frames] conj (assoc frame :ball1 5 :ball2 5 :score 10))
+          nxf (assoc frame :ball1 5 :ball2 3 :score 8)
+          sc2 (update-last-frame sc1 nxf)]
+      (is (= 15 (-> sc2 :frames (nth 0) :score)))))
+
+  (testing "score-frame with strikes"
+    (let [sc1 (update-in score-card [:frames] conj (assoc frame :ball1 10 :ball2 0 :score 10))
+          nxf (assoc frame :ball1 7 :ball2 3 :score 10)
+          sc2 (update-last-frame sc1 nxf)]
+      (is (= 20 (-> sc2 :frames (nth 0) :score)))
+
+      ;; simulating a strike in the 1st 2 frames final score for frame1 should be updated along
+      ;; with final score of frame2
+      (let [sc1 (score-frame score-card 10 0)]              ;; add first frame
+        (is (= 1 (-> sc1 :frames count)))
+        (is (= 10 (-> sc1 :frames (nth 0) :score)))
+        (is (= 10 (get-total-score sc1)))
+
+        (let [sc2 (score-frame sc1 10 0)]
+          (is (= 2 (-> sc2 :frames count)))
+          (is (= 20 (-> sc2 :frames (nth 0) :score)))
+          (is (= 10 (-> sc2 :frames (nth 1) :score)))
+          (is (= 30 (get-total-score sc2)))
+
+          (let [sc3 (score-frame sc2 7 0)]
+            (is (= 3 (-> sc3 :frames count)))
+            (is (= 27 (-> sc3 :frames (nth 0) :score)))
+            (is (= 17 (-> sc3 :frames (nth 1) :score)))
+            (is (= 51 (get-total-score sc3)))
+
+            (let [sc4 (score-frame sc3 0 4)]
+              (is (= 4 (-> sc4 :frames count)))
+              (is (= 17 (-> sc4 :frames (nth 1) :score)))
+              (is (= 7 (-> sc4 :frames (nth 2) :score)))
+              (is (= 4 (-> sc4 :frames (nth 3) :score)))
+              (is (= 55 (get-total-score sc4)))
+
+              (let [sc5 (score-frame sc4 10 0)]
+                (is (= 4 (-> sc5 :frames (nth 3) :score)))
+                (is (= 10 (-> sc5 :frames (nth 4) :score)))
+                (is (= 65 (get-total-score sc5)))
+
+                (let [sc6 (score-frame sc5 5 4)]
+                  (is (= 19 (-> sc6 :frames (nth 4) :score)))
+                  (is (= 9 (-> sc6 :frames (nth 5) :score)))
+                  (is (= 83 (get-total-score sc6)))
+
+                  (let [sc7 (score-frame sc6 7 3)]
+                    (is (= 93 (get-total-score sc7)))
+
+                    (let [sc8 (score-frame sc7 3 3)]
+                      (is (= 13 (-> sc8 :frames (nth 6) :score)))
+                      (is (= 102 (get-total-score sc8)))
+
+                      (let [sc9 (score-frame sc8 10 0)]
+                        (is (= 112 (get-total-score sc9)))
+
+                        (let [sc10 (score-frame sc9 10 7 1)]
+                          (is (= 27 (-> sc10 :frames (nth 8) :score)))
+                          (is (= 18 (-> sc10 :frames (nth 9) :score)))
+                          (is (= 147 (get-total-score sc10))))))))))))))))
